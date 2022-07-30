ee11e44c18485b2f6a1abd1105c99af6c01f0d19
Nadezhda <nan.dudova@gmail.com>
2022-07-30 19:10:24 +0300

    commit
    commit

---
+++ src/JOOP15UnitTestingLab/JOOP16UnitTestingExercises/main/p06_TirePressureMonitoringSystem/Main.java
@@ -0,0 +1,9 @@
+package main.p06_TirePressureMonitoringSystem;
+
+/*
+Problem 6.	Tire Pressure Monitoring System
+You are given a small project for a system that monitors the pressure in car tires. Your task is to write unit tests for the system. You will need to use mocking to pass dependencies. Think about the corner cases of the project.
+ */
+
+public class Main {
+}
