697579f2ec3bd0c897fd2a06141b827525c171e8
Nadezhda <nan.dudova@gmail.com>
2022-07-30 19:13:05 +0300

    commit
    commit

---
+++ src/JOOP16UnitTestingExercises/test/p02_ExtendedDatabase/DatabaseTest.java
@@ -0,0 +1,70 @@
+package p02_ExtendedDatabase;
+
+import main.p02_ExtendedDatabase.Database;
+import main.p02_ExtendedDatabase.Person;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.naming.OperationNotSupportedException;
+
+public class DatabaseTest {
+//    private Database database;
+//
+//    @Before
+//    public void prepare() throws OperationNotSupportedException {
+//        Person person1 = new Person(14, "Pesho");
+//        Person person2 = new Person(25, "Gosho");
+//
+//        database = new Database(person1, person2);
+//    }
+
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testFindByUsernameWithNullShouldThrow() throws OperationNotSupportedException {
+        Person person1 = new Person(14, "Pesho");
+        Person person2 = new Person(25, "Gosho");
+        Database database = new Database(person1, person2);
+
+        database.findByUsername(null);
+    }
+
+    @Test
+    public void testFindByUsernameSuccess() throws OperationNotSupportedException {
+        Person person1 = new Person(14, "Pesho");
+        Person person2 = new Person(25, "Gosho");
+        Database database = new Database(person1, person2);
+
+        Person personAfterMethod = database.findByUsername("Pesho");
+        Assert.assertEquals(person1.getUsername(), personAfterMethod.getUsername());
+        Assert.assertEquals(person1.getId(), personAfterMethod.getId());
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testFindByUsernameWithMissingName() throws OperationNotSupportedException {
+        Person person1 = new Person(14, "Pesho");
+        Person person2 = new Person(25, "Gosho");
+        Database database = new Database(person1, person2);
+
+        database.findByUsername("Tosho");
+    }
+
+    @Test
+    public void findByIdSuccess() throws OperationNotSupportedException {
+        Person person1 = new Person(14, "Pesho");
+        Person person2 = new Person(25, "Gosho");
+        Database database = new Database(person1, person2);
+
+        Person personAfterMethod = database.findById(14);
+        Assert.assertEquals(14, personAfterMethod.getId());
+        Assert.assertEquals("Pesho", personAfterMethod.getUsername());
+    }
+
+    @Test(expected = OperationNotSupportedException.class)
+    public void testFindByIdWithMissingIdThrows() throws OperationNotSupportedException {
+        Person person1 = new Person(14, "Pesho");
+        Person person2 = new Person(25, "Gosho");
+        Database database = new Database(person1, person2);
+
+        database.findById(16);
+    }
+}
---
+++ src/JOOP16UnitTestingExercises/test/p06_TirePressureMonitoringSystem/AlarmTest.java
@@ -0,0 +1,43 @@
+package p06_TirePressureMonitoringSystem;
+
+import main.p06_TirePressureMonitoringSystem.Alarm;
+import main.p06_TirePressureMonitoringSystem.Sensor;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class AlarmTest {
+
+    @Test
+    public void testAlarmWithLowPressure() {
+        //Sensor sensor = new Sensor(); -> не, защото сензора е рандом true рандом false
+        Sensor sensor = Mockito.mock(Sensor.class);
+
+        //предефинирам това, което искам сензора да ми върне
+        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(12.0);
+
+        Alarm alarm = new Alarm(sensor);
+        alarm.check();
+        Assert.assertTrue(alarm.getAlarmOn());
+    }
+
+    @Test
+    public void testAlarmWithHighPressure() {
+        Sensor sensor= Mockito.mock(Sensor.class);
+        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(12000.0);
+
+        Alarm alarm = new Alarm(sensor);
+        alarm.check();
+        Assert.assertTrue(alarm.getAlarmOn());
+    }
+
+    @Test
+    public void testAlarmWithNormalPressure() {
+        Sensor sensor= Mockito.mock(Sensor.class);
+        Mockito.when(sensor.popNextPressurePsiValue()).thenReturn(20.0);
+
+        Alarm alarm = new Alarm(sensor);
+        alarm.check();
+        Assert.assertFalse(alarm.getAlarmOn());
+    }
+}
