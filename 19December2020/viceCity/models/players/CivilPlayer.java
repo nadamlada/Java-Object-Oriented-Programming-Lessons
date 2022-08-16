caaca16e94ff38af6278e8653138ceee0ea558df
Nadezhda <nan.dudova@gmail.com>
2022-08-16 15:40:48 +0300

    Delete 19December2020/viceCity directory

--- 19December2020/viceCity/models/players/CivilPlayer.java
+++
@@ -1,9 +0,0 @@
-package viceCity.models.players;
-
-public class CivilPlayer extends BasePlayer {
-    private static final int CIVIL_INITIAL_POINTS = 50;
-
-    public CivilPlayer(String name) {
-        super(name, CIVIL_INITIAL_POINTS);
-    }
-}
