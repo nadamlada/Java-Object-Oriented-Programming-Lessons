1717270a694b9bab6c0af6a3dd5a996e425eb323
Nadezhda <nan.dudova@gmail.com>
2022-08-26 13:02:12 +0300

    commit

--- src/JOOP02WorkingWithAbstractionExercises/X01CardSuit/Main.java
+++ src/JOOP02WorkingWithAbstractionExercises/X01CardSuit/Main.java
@@ -1,4 +1,4 @@
-package JOOP01WorkingWithAbstractionExercise.EX01CardSuit;
+package JOOP02WorkingWithAbstractionExercises.X01CardSuit;
 
 /*
 Create an enumeration type that has as its constants the four suits of a deck of playing cards (CLUBS, DIAMONDS, HEARTS, SPADES). Iterate over the values of the enumeration type and print all ordinal values and names.
