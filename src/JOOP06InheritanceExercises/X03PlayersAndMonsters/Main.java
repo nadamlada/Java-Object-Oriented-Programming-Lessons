630138eadd3d76904952b71d0b37df3560e07907
Nadezhda <nan.dudova@gmail.com>
2022-09-14 14:16:04 +0300

    Add files via upload

---
+++ src/JOOP06InheritanceExercises/X03PlayersAndMonsters/Main.java
@@ -0,0 +1,19 @@
+package hero;
+
+/*
+NOTE: You need a public class Main. Create a package hero.
+Your task is to create the following game hierarchy:
+Create a class Hero. It should contain the following members:
+•	A public constructor, which accepts:
+o	username – String
+o	level – int
+•	The following fields:
+o	username - String
+o	level – int
+•	Getters for username and level
+•	toString() method
+Hint: Override toString() of the base class in the following way:
+ */
+
+public class Main {
+}
